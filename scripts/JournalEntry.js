e916e7d4e05d40b5a3a9d81e28ab2592a1628699
Chris Murphy <theworldisyours@gmail.com>
2021-09-03 17:07:17 -0400

    created journalData, JournalEntry, and modified main.css styling

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -6,11 +6,15 @@ export const JournalEntryComponent = (entry) => {
     return `
         <section class="journalEntryContainer" id="entry--${entry.id}" class="journalEntry">
             <div class="journalEntry">   
-                <p>Date: ${entry.date}</p>
-                <p>Concepts Covered: ${entry.concept}</p>
-                <p>Journal Entry: ${entry.entry}</p>
-                <p>Mood: ${entry.mood}</p>
+            <p>Date: ${entry.date}</p>
+            <p>Concepts Covered: ${entry.concept}</p>
+            <p>Journal Entry: ${entry.entry}</p>
+            <p>Mood: ${entry.mood}</p>
             </div>
         </section>
     `
 }
+
+
+
+
